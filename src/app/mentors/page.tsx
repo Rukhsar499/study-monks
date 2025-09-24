371d3fbfa45a51caaf1c8a1072fc41f66e8f3a62
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2025-09-24 17:03:17 +0530

    hii

--- src/app/mentors/page.tsx
+++ src/app/mentors/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
                 <div className="max-w-7xl container mx-auto px-4">
                     <h2 className="text-[36px] font-bold text-center">Meet the Wise Guardians</h2>
                     <p className="text-center text-[14px] text-[#484848]">
-                        Our mentors are the living embodiment of our philosophy. They are handpicked not only for their deep subject expertise but for their rare ability to guide and inspire. We believe so strongly in this that <span className="text-[#003c79] font-bold">only one out of 5 applicants</span> meet our rigorous standards for both academic excellence and empathetic guidance. Below, you will find dedicated partners for your child's educational journey.
+                        Our mentors are the living embodiment of our philosophy. They are handpicked not only for their deep subject expertise but for their rare ability to guide and inspire. We believe so strongly in this that <span className="text-[#003c79] font-bold">only one out of 5 applicants</span> meet our rigorous standards for both academic excellence and empathetic guidance. Below, you will find dedicated partners for your child&apos;s educational journey.
                     </p>
                 </div>
             </section>
