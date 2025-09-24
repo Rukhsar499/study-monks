eb373510dbc63e7f4f835ec831cc4eeaa0f0bb89
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2025-09-24 19:35:26 +0530

    hii

--- src/app/mentors/page.tsx
+++ src/app/mentors/page.tsx
@@ -29,40 +29,9 @@ export default function Page() {
                     </p>
                 </div>
             </section>
-
+            <section className="container mx-auto max-w-7xl px-4mentr bg-[url('/assets/img/MONKS.webp')] bg-cover bg-center min-h-[280px] flex items-center"></section>
             <Teacher />
-            {/* <section className="mb-[30px] md:mb-[100px] mt-[100px] relative">
-                <div className="absolute top-0 left-80 hidden md:block">
-                    <Image
-                        src="/assets/img/l-bh.png"
-                        alt="Mother and Child"
-                        width={250}
-                        height={250}
-                        className="z-10"
-                    />
-                </div>
-                <div className="max-w-7xl container mx-auto px-4">
-                    <h2 className="text-[36px] font-bold text-center mb-5">Ready to Find the Perfect Teacher?</h2>
-                    <p className="text-center text-[16px] text-[#484848]">
-                        Schedule a complimentary Clarity Session to speak with one of our academic counselors and discuss how we can tailor a learning path for your child.
-                    </p>
-                    <div className="flex justify-center items-center mt-5">
-                        <button className="bg-[#B2BF4D] text-[#000] text-left text-[16px] px-8 py-3 rounded-3xl"
-                            style={{ boxShadow: "0 4px 4px rgba(0, 0, 0, .39)" }}>
-                            Schedule a clarity session
-                        </button>
-                    </div>
-                </div>
-                <div className="absolute top-0 right-80 hidden md:block">
-                    <Image
-                        src="/assets/img/r-bh.png"
-                        alt="Mother and Child"
-                        width={250}
-                        height={250}
-                        className="z-10"
-                    />
-                </div>
-            </section> */}
+
             <Section
                 title="Ready to Find the Perfect Teacher?"
                 description="Schedule a complimentary Clarity Session to speak with one of our academic counselors and discuss how we can tailor a learning path for your child."
